05277cde55f55e173a9722c368c008c6256a070a
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-09-19 10:12:35 -0600

    Adds documentation to model, modelController, viewController, && tableViewCell files to detail steps. Additionally, restructures code to be formatted correctly

--- SettingsMiniProject/SettingsViewController.swift
+++ SettingsMiniProject/SettingsViewController.swift
@@ -8,30 +8,51 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SettingTableViewCellDelegate {
-    
+class SettingsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-	
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Eliminate extra cells from the tableView
+        tableView.tableFooterView = UIView()
+    }
+}
+
+// MARK: - TableViewDataSource && TableViewDelegate Conformance
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return SettingsController.shared.mySettings.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell") as? SettingTableViewCell
-
+        
         let setting = SettingsController.shared.mySettings[(indexPath as NSIndexPath).row]
-		cell?.setting = setting
+        cell?.setting = setting
+        // Step 5 of 5 for Custom Delegate - set your delegate = self
+        /// Assign the SettingTableViewCell property of delegate to self (refering to SettingsViewController). The SettingsViewController can now handle the SettingTableViewCellDelegate methods.
+        /// - Requires: Class confromance to the protocol.
         cell?.delegate = self
         return cell ?? UITableViewCell()
     }
-    
-    func settingValueChanged(_ cell: SettingTableViewCell, selected: Bool) {
-        guard let setting = cell.setting,
-            let cellIndexPath = tableView.indexPath(for: cell) else {return}
-        tableView.beginUpdates()
-		setting.isSet = selected
-        tableView.reloadRows(at: [cellIndexPath], with: .automatic)
-        tableView.endUpdates()
-    }
+}
 
+// MARK: - SettingTableViewCellDelegate Conformance
+// Step 4 of 5 in Custom Delegate - conform to your custom protocol
+extension SettingsViewController: SettingTableViewCellDelegate {
+    /**
+     Conformance to the SettingTableViewCellDelegate protocol
+     
+     - Note:
+     Here the SettingsViewController is conforming to the SettingTableViewCellDelegate protocol. The class is required to handle its protocol method, settingSwitchValueChanged(_ cell:,selected:). When the method is called, run any required logic.
+     
+     - Parameters:
+        - cell: The cell that is calling the function
+        - selected: The current state of the UISwitch on the cell
+     */
+    func settingValueChanged(_ cell: SettingTableViewCell, selected: Bool) {
+        guard let setting = cell.setting else {return}
+        setting.isSet = selected
+        cell.updateViews()
+    }
 }

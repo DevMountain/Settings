630aa98155c65357bb2bd07e81c6386e4cf2f019
Karl Pfister <pfister@Karls-MacBook-Pro.local>
2020-02-14 15:33:01 -0700

    Resets starter
    - Removed older project
    + Added images to Assets.xcassets

---
+++ Settings/Settings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Karl Pfister on 2/14/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
